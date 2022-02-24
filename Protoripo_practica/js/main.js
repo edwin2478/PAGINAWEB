99f28fb7f921f1d510788373b63c9e3cd83d3403
edwin2478 <75391817+edwin2478@users.noreply.github.com>
2022-02-24 12:19:09 -0500

    first commit

---
+++ Protoripo_practica/js/main.js
@@ -0,0 +1,35 @@
+/* Carga */
+
+window.onload = function(){
+    $('#onload').fadeOut();
+    $('body').removeClass('hidden');
+}
+
+
+
+
+function redirectToOtherPage(url) {
+    window.location.href = url;
+}
+$(document).ready(function () {
+    $("#on").click(function(){
+        $("#Cuadrado").animate({left: "500px", height: "200px", width: "200px"},5000);
+});
+
+$("#btnVerDetalles").click(function(){
+  $(this).append('<span></span>')
+  var top =e.pageY;
+  var left =e.pageY;
+  $(this).children().offset({top:top, left:left}).fadeOut(500);
+});
+
+$(".nav-link active").click(function(){
+    $(this).css({"color" : "black"});
+})
+$("#zoom").elevatezoom({zoomType:'Lens', lenshade:'round',lensize:80});
+
+
+});
+
+
+
